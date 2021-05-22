b61b77dfa6997c8c4bbf1b865795878fc0125181
Sahidul Arif <neptunbd@gmail.com>
2021-05-22 11:51:40 +0600

    final commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs-extra');
+require('dotenv').config();
+const app = express();
+
+const port1 = 3500
+const port2 = 4000
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const routes = require('./routes/routes.js')(app, fs);
+
+// Root:
+app.get('/', (req, res) => {
+  res.send('The Food Bots Server is running');
+});
+
+// Listener port
+app.listen(port1 || port2);
